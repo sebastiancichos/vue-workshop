a39357c50fdd05e15ca76b294efd46c75c5b335b
Wojciech Urbanski <wojciech.urbanski@senti1.com>
2017-06-25 18:41:48 +0200

    12. First component, navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav class="navbar container">
+      <a class="navbar--brand"
+         href="#"
+         :title="appName"
+      >
+        {{ appName }}
+      </a>
+      <ul class="navbar--menu">
+        <li class="active"><a href="#">Somewhere</a></li>
+        <li class="active"><a href="#">Over</a></li>
+        <li class="active"><a href="#">The rainbow</a></li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        appName: "Vue.js (work)shop"
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../assets/style";
+
+  .navbar {
+    @extend .box;
+    line-height: asRem(30px);
+    @include clearfix();
+
+    &--brand {
+      font-size: asRem(20px);
+      float: left;
+      margin-right: $gutter;
+      color: $c-black;
+      &:hover {
+        text-decoration: none;
+      }
+    }
+
+    &--menu {
+      float: left;
+      list-style: none;
+      padding: 0;
+      margin: 0;
+
+      li {
+        display: inline-block;
+        margin-right: $gutter/2;
+      }
+    }
+  }
+</style>
