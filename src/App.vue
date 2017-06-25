e1ea48b004faddd1c3f88abd3418e07c972a05c4
Wojciech Urbanski <wojciech.urbanski@senti1.com>
2017-06-25 18:16:04 +0200

    6. v-for used to display products and materials

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,12 @@
 
     <section class="container">
       <ul class="product-list">
-        <li class="product-list--product" v-for="n in page">
+        <li class="product-list--product" v-for="product in products">
           <div class="ribbon" style="color: orangered;" />
             <img class="product-list--product--image" :src="product.photo" alt=""/>
             <div class="product-list--product--caption">
               <h4 class="product-list--product--name">
-                {{ n }} {{ product.name }}
+                {{ product.name }}
               </h4>
               <p class="product-list--product--description">
                 {{ product.description }}
@@ -75,9 +75,9 @@
               <dt>Materials:</dt>
               <dd>
                 <ul class="product--materials">
-                  <li>wood</li>
-                  <li>metal</li>
-                  <li>plastic</li>
+                  <li v-for="material in product.materials">
+                    {{ material }}
+                  </li>
                 </ul>
               </dd>
 
@@ -91,10 +91,10 @@
                 ${{product.price }} <span v-show="product.price > 20" class="lozenge">free shipping</span>
               </dd>
             </dl>
-            <div class="product--footer">
-              <div class="product--actions">
-                <a class="btn" href="#">Edit product</a>
-              </div>
+          </div>
+          <div class="product--footer">
+            <div class="product--actions">
+              <a class="btn" href="#">Edit product</a>
             </div>
           </div>
       </article>
@@ -214,7 +214,66 @@
             "Granite",
             "Plastic"
           ]
-        }
+        },
+        products: [
+          {
+            "id": 1,
+            "name": "Intelligent Soft Computer",
+            "price": "280.00",
+            "department": "Health",
+            "color": "#126123",
+            "description": "Est occaecati porro. Qui voluptatum nihil et voluptatem ea. Ut repellendus dicta velit quisquam totam sequi dolorum. Eius voluptatem asperiores consequatur non iusto atque et mollitia. Omnis sequi repudiandae.",
+            "photo": "https://unsplash.it/400/250?image=71",
+            "inStock": 7,
+            "materials": [
+              "Fresh"
+            ]
+          },
+          {
+            "id": 3,
+            "name": "Awesome Granite Salad",
+            "price": "809.00",
+            "department": "Movies",
+            "color": "#402a23",
+            "description": "Ut ut dolores ut aut officia qui nemo eum voluptatem. Voluptas dolores qui voluptatem consequatur quam itaque. Ullam voluptatem in dolorem repellat non aut vero ea.",
+            "photo": "https://unsplash.it/400/250?image=212",
+            "inStock": 10,
+            "materials": [
+              "Rubber"
+            ]
+          },
+          {
+            "id": 4,
+            "name": "Rustic Cotton Cheese",
+            "price": "601.00",
+            "department": "Health",
+            "color": "#18047b",
+            "description": "Et inventore qui doloremque vel. Ut nulla quae nobis aut. Ipsam ut impedit sed consequatur illo neque alias ipsum.",
+            "photo": "https://unsplash.it/400/250?image=1000",
+            "inStock": 6,
+            "materials": [
+              "Wooden",
+              "Metal",
+              "Frozen",
+              "Soft",
+              "Rubber"
+            ]
+          },
+          {
+            "id": 5,
+            "name": "My updated product",
+            "price": "219.00",
+            "department": "Home",
+            "color": "#51452b",
+            "description": "Consequatur nam distinctio modi et omnis odio optio. Ut ipsum voluptatem. Voluptatem est voluptatem a. Sed et et sunt aut. Error hic dolor quas velit aut omnis repudiandae.",
+            "photo": "https://unsplash.it/400/250?image=501",
+            "inStock": 3,
+            "materials": [
+              "Granite",
+              "Plastic"
+            ]
+          }
+        ]
       }
     },
     methods: {
